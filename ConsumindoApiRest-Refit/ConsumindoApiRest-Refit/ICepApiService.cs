15db28bfe559b586ecbedbf9921effef7a8b7529
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-05-31 13:34:50 -0300

    Consumindo API

---
+++ ConsumindoApiRest-Refit/ConsumindoApiRest-Refit/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ConsumindoApiRest_Refit
+{
+    interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ ConsumindoApiRest-Refit/ConsumindoApiRest-Refit/Program.cs
@@ -0,0 +1,32 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsumindoApiRest_Refit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepCliente = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.Write("Informe seu cep: ");
+                string cepInformado = Console.ReadLine().ToString();
+
+                Console.WriteLine($"Consultando informações do CEP: {cepInformado}");
+
+                var address = await cepCliente.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro: {address.Logradouro} " +
+                    $"\nBairro: {address.Bairro} " +
+                    $"\nCidade: {address.Localidade}");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Erro na consulta de cep? {e.Message}");
+            }
+        }
+    }
+}
